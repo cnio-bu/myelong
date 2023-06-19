28aeea02f5a9d5bbcf0902a268247b7ee1a0d9da
Alessandro Gostoli <ale.gostoli@gmail.com>
2023-06-19 23:42:07 +0200

    hardcoded paths fix

--- workflow/scripts/variants_analysis.sh
+++ workflow/scripts/variants_analysis.sh
@@ -2,33 +2,33 @@
 sniffles_path='/storage/scratch01/groups/bu/myeloma_altum/agostoli/board_alegosto01/myelong/results_concatenated_samples/sniffles/patient_sniffles.vcf'
 cutesv_path='/storage/scratch01/groups/bu/myeloma_altum/agostoli/board_alegosto01/myelong/results_concatenated_samples/cutesv/patient_cutesv.vcf'
 
-printf "INFO \t SNIFFLES \t CUTE\n" > bcftools/analysis.tsv
+printf "INFO \t SNIFFLES \t CUTE\n" > "${snakemake_output[0]}"
 
-total_variants_snif=$(bcftools query -f '%CHROM\n' $sniffles_path | grep -v -c '^#')
-total_variants_cute=$(bcftools query -f '%CHROM\n' $cutesv_path | grep -v -c '^#')
+total_variants_snif=$(bcftools query -f '%CHROM\n' "${snakemake_input[sniffles]}" | grep -v -c '^#')
+total_variants_cute=$(bcftools query -f '%CHROM\n' "${snakemake_input[cutesv]}" | grep -v -c '^#')
 
-chromlist=($(bcftools query -f '%CHROM\n' $sniffles_path| uniq|grep 'chr'))
+chromlist=($(bcftools query -f '%CHROM\n' "${snakemake_input[sniffles]}"| uniq|grep 'chr'))
 
-snps_snif=$(bcftools view -v snps $sniffles_path |grep -v -c '^#')
-snps_cute=$(bcftools view -v snps $cutesv_path |grep -v -c '^#')
+snps_snif=$(bcftools view -v snps "${snakemake_input[sniffles]}" |grep -v -c '^#')
+snps_cute=$(bcftools view -v snps "${snakemake_input[cutesv]}" |grep -v -c '^#')
 
-indels_snif=$(bcftools view -v indels $sniffles_path |grep -v -c '^#')
-indels_cute=$(bcftools view -v indels $cutesv_path |grep -v -c '^#')
+indels_snif=$(bcftools view -v indels "${snakemake_input[sniffles]}" |grep -v -c '^#')
+indels_cute=$(bcftools view -v indels "${snakemake_input[cutesv]}" |grep -v -c '^#')
 
-bnd_snif=$(bcftools view -v bnd $sniffles_path |grep -v -c '^#')
-bnd_cute=$(bcftools view -v bnd $cutesv_path |grep -v -c '^#')
+bnd_snif=$(bcftools view -v bnd "${snakemake_input[sniffles]}" |grep -v -c '^#')
+bnd_cute=$(bcftools view -v bnd "${snakemake_input[cutesv]}" |grep -v -c '^#')
 
-del_snif=$(bcftools query -f '%ID\n' $sniffles_path |grep 'Sniffles2.DEL'| grep -v -c '^#')
-del_cute=$(bcftools query -f '%ID\n' $cutesv_path |grep 'cuteSV.DEL'| grep -v -c '^#')
+del_snif=$(bcftools query -f '%ID\n' "${snakemake_input[sniffles]}" |grep 'Sniffles2.DEL'| grep -v -c '^#')
+del_cute=$(bcftools query -f '%ID\n' "${snakemake_input[cutesv]}" |grep 'cuteSV.DEL'| grep -v -c '^#')
 
-ins_snif=$(bcftools query -f '%ID\n' $sniffles_path |grep 'Sniffles2.INS'| grep -v -c '^#')
-ins_cute=$(bcftools query -f '%ID\n' $cutesv_path |grep 'cuteSV.INS'| grep -v -c '^#')
+ins_snif=$(bcftools query -f '%ID\n' "${snakemake_input[sniffles]}" |grep 'Sniffles2.INS'| grep -v -c '^#')
+ins_cute=$(bcftools query -f '%ID\n' "${snakemake_input[cutesv]}" |grep 'cuteSV.INS'| grep -v -c '^#')
 
-inv_snif=$(bcftools query -f '%ID\n' $sniffles_path |grep 'Sniffles2.INV'| grep -v -c '^#')
-inv_cute=$(bcftools query -f '%ID\n' $cutesv_path |grep 'cuteSV.INV'| grep -v -c '^#')
+inv_snif=$(bcftools query -f '%ID\n' "${snakemake_input[sniffles]}" |grep 'Sniffles2.INV'| grep -v -c '^#')
+inv_cute=$(bcftools query -f '%ID\n' "${snakemake_input[cutesv]}" |grep 'cuteSV.INV'| grep -v -c '^#')
 
-dup_snif=$(bcftools query -f '%ID\n' $sniffles_path |grep 'Sniffles2.DUP'| grep -v -c '^#')
-dup_cute=$(bcftools query -f '%ID\n' $cutesv_path |grep 'cuteSV.DUP'| grep -v -c '^#')
+dup_snif=$(bcftools query -f '%ID\n' "${snakemake_input[sniffles]}" |grep 'Sniffles2.DUP'| grep -v -c '^#')
+dup_cute=$(bcftools query -f '%ID\n' "${snakemake_input[cutesv]}" |grep 'cuteSV.DUP'| grep -v -c '^#')
 
 del_snif_float=$del_snif.0
 del_cute_float=$del_cute.0
@@ -42,20 +42,20 @@ indels_ratio_cute=$(awk "BEGIN {print $del_cute_float/$ins_cute_float}")
 #indels_ratio_cute=$(($del_cute_float / $ins_cute_float))
 
 
-printf "total variants \t $total_variants_snif \t $total_variants_cute \n" >> results/bcftools/analysis.tsv
+printf "total variants \t $total_variants_snif \t $total_variants_cute \n" >> "${snakemake_output[0]}"
 
 for chrom in ${chromlist[@]}
 do
-count_snif=$(bcftools view -t $chrom $sniffles_path | grep -v -c '^#')
-count_cute=$(bcftools view -t $chrom $cutesv_path | grep -v -c '^#')
-printf "$chrom\t$count_snif\t$count_cute\n" >> results/bcftools/analysis.tsv
+count_snif=$(bcftools view -t $chrom "${snakemake_input[sniffles]}" | grep -v -c '^#')
+count_cute=$(bcftools view -t $chrom "${snakemake_input[cutesv]}" | grep -v -c '^#')
+printf "$chrom\t$count_snif\t$count_cute\n" >> "${snakemake_output[0]}"
 done
 
-printf "SNPs count\t $snps_snif \t $snps_cute\n" >> results/bcftools/analysis.tsv
-printf "Indels count\t $indels_snif \t $indels_cute\n" >> results/bcftools/analysis.tsv
-printf "Deletions count\t $del_snif \t $del_cute\n" >> results/bcftools/analysis.tsv
-printf "Insertions count\t $ins_snif \t $ins_cute\n" >> results/bcftools/analysis.tsv
-printf "Inversions count\t $inv_snif \t $inv_cute\n" >> results/bcftools/analysis.tsv
-printf "Breakends count\t $bnd_snif \t $bnd_cute\n" >> results/bcftools/analysis.tsv
-printf "Duplications count\t $dup_snif \t $dup_cute\n" >> results/bcftools/analysis.tsv
-printf "DEL/INS ratio\t $indels_ratio_snif \t $indels_ratio_cute\n" >> results/bcftools/analysis.tsv
+printf "SNPs count\t $snps_snif \t $snps_cute\n" >> "${snakemake_output[0]}"
+printf "Indels count\t $indels_snif \t $indels_cute\n" >> "${snakemake_output[0]}"
+printf "Deletions count\t $del_snif \t $del_cute\n" >> "${snakemake_output[0]}"
+printf "Insertions count\t $ins_snif \t $ins_cute\n" >> "${snakemake_output[0]}"
+printf "Inversions count\t $inv_snif \t $inv_cute\n" >> "${snakemake_output[0]}"
+printf "Breakends count\t $bnd_snif \t $bnd_cute\n" >> "${snakemake_output[0]}"
+printf "Duplications count\t $dup_snif \t $dup_cute\n" >> "${snakemake_output[0]}"
+printf "DEL/INS ratio\t $indels_ratio_snif \t $indels_ratio_cute\n" >> "${snakemake_output[0]}"
